5e989cc5fd492781afa18c55ceb45457cbb8e2e1
huayang wang <2586244885@qq.com>
2023-06-25 09:45:29 +0800

    init

--- README.md
+++ README.md
@@ -1,15 +1,7 @@
-# pkg-name
+# @warmthsea/utils
 
-[![NPM version](https://img.shields.io/npm/v/pkg-name?color=a1b858&label=)](https://www.npmjs.com/package/pkg-name)
-
-## Sponsors
-
-<p align="center">
-  <a href="https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg">
-    <img src='https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg'/>
-  </a>
-</p>
+[![NPM version](https://img.shields.io/npm/v/@warmthsea/utils?color=a1b858&label=)](https://www.npmjs.com/package/@warmthsea/utils)
 
 ## License
 
-[MIT](./LICENSE) License © 2022 [Anthony Fu](https://github.com/antfu)
+[MIT](./LICENSE) License © 2023 [Warmthsea](https://github.com/warmthsea)
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,47 @@
-export const one = 1
-export const two = 2
+import FormData from 'form-data'
+
+/**
+ * 异步等待时间
+ * @param timer ms
+ * @returns
+ */
+export function utilAwaitTime(timer: number): Promise<void> {
+  return new Promise((resolve) => {
+    const timers: NodeJS.Timeout = setTimeout(() => {
+      clearTimeout(timers)
+      resolve()
+    }, timer)
+  })
+}
+
+export const utilDelay = (function () {
+  let timer: NodeJS.Timeout
+  return function (callback: () => void, ms: number | undefined) {
+    clearTimeout(timer)
+    timer = setTimeout(callback, ms)
+  }
+})()
+
+/** 获取文件地址后缀 */
+export function utilGetSuffix(str: string): string {
+  if (!str.includes('.'))
+    return ''
+  const fileExtension = str.substring(str.lastIndexOf('.') + 1)
+  return fileExtension
+}
+
+/** Object 转化 为 FormData */
+export function utilFormData<T extends Record<string, any>>(data: T): FormData {
+  const formData = new FormData()
+  for (const item in data)
+    data[item] !== undefined && formData.append(item, data[item])
+
+  return formData
+}
+
+/** Object 数据为空的清除 */
+export function utilObjectFilter<T extends Record<string, any>>(data: T): T {
+  const parData = { ...data }
+  for (const item in parData) !parData[item] && delete parData[item]
+  return parData
+}
